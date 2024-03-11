bad403c836ef44dac0bde6996edd274cce24ab13
Bamberek <kabaczek99@gmail.com>
2024-03-11 20:23:53 +0100

    updated currency form

--- src/components/Form/CurrencyForm.jsx
+++ src/components/Form/CurrencyForm.jsx
@@ -1,18 +1,55 @@
 import { useState } from "react";
 import { BsCurrencyExchange } from "react-icons/bs";
-import Loader from "../Loader/Loader";
+import Loader from "./Loader/Loader";
 import AmountInput from "./AmountInput/AmountInput";
 import CurrencySelection from "./CurrencySelection/CurrencySelection ";
+import clsx from "clsx";
 
-const CurrencyForm = ({
-  convertCurrency,
-  clearApp,
-  loading,
-  result,
-  amount,
-  setAmount,
-}) => {
+const CurrencyForm = () => {
+  const [result, setResult] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [amount, setAmount] = useState("");
   const [currencyCode, setCurrencyCode] = useState("EUR");
+  const [resultColor, setResultColor] = useState("");
+
+  const convertCurrency = async (amount, currencyCode) => {
+    setLoading(true);
+    setResultColor("");
+
+    if (!amount || isNaN(amount) || amount <= 0) {
+      setResult("Enter a valid amount");
+      setResultColor("text-danger");
+      setLoading(false);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        `https://api.nbp.pl/api/exchangerates/rates/A/${currencyCode}/?format=json`
+      );
+      const data = await response.json();
+
+      const exchangeRate = data?.rates?.[0]?.mid;
+
+      if (!exchangeRate) {
+        throw new Error("Exchange rate not available.");
+      }
+
+      const convertedResult = (amount * exchangeRate).toFixed(2);
+
+      setResult(`${amount} ${currencyCode} = ${convertedResult} PLN`);
+    } catch (error) {
+      setResult("Exchange rate not available. Please try again");
+      setResultColor("text-danger");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const clearApp = () => {
+    setResult("");
+    setAmount("");
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -48,7 +85,7 @@ const CurrencyForm = ({
         <AmountInput amount={amount} setAmount={setAmount} />
       </form>
 
-      <p className="fs-2" id="result">
+      <p className={clsx("fs-2", resultColor)} id="result">
         {result}
       </p>
       <Loader loading={loading} />
